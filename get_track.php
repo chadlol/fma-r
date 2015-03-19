e7942213b0ace97683bd5d8c3704a33079caf988
chadlol <chadohl@gmail.com>
2015-03-19 06:54:04 -0700

    Added Album Art + Header

--- get_track.php
+++ get_track.php
@@ -1,15 +1,21 @@
 <?php
 
 $page = rand(1,4057);
-$track_id = rand(0, 19);
+$id = rand(0, 19);
 
 $url = "http://freemusicarchive.org/api/get/tracks.json?api_key=7Y2HJLL7FFMODARC&page=$page";
 $content = file_get_contents($url);
 $json = json_decode($content, true);
 
-$track_id = $json['dataset'][$track_id]['track_id'];
+$track_id = $json['dataset'][$id]['track_id'];
+$track_art = $json['dataset'][$id]['track_image_file'];
+
+$artist_name = $json['dataset'][$id]['artist_name'];
 
 $player = <<<EOD
+    <div id='track_art'>
+        <img src='$track_art' alt='$artist_name'/>
+    </div>
     <object width="300" height="50"><param name="movie" value="http://freemusicarchive.org/swf/trackplayer.swf"/>
         <param name="flashvars" value="track=http://freemusicarchive.org/services/playlists/embed/track/$track_id.xml"/>
         <param name="allowscriptaccess" value="sameDomain"/>
@@ -18,5 +24,4 @@ $player = <<<EOD
 EOD;
 
 echo $player;
-
 ?>
--- index.php
+++ index.php
@@ -7,50 +7,82 @@
     <title>FMA-r</title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <link rel="stylesheet" href="stylesheets/btns.min.css" >
+    <link href='http://fonts.googleapis.com/css?family=Quicksand:300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="stylesheets/btns.min.css">
 
     <style>
-        #box{
+        #header{
+            font-family: 'Quicksand', sans-serif;
+            font-size: 64pt;
+            text-align: center;
+        }
+        #box {
             width: 300px;
-            height: 50px;
+            height: 400px;
             margin: auto;
-            position: absolute;
-            top: 0; left: 0; bottom: 0; right: 0;
         }
-        #media{
-            height:50px;
 
+        #media {
+            height: auto
         }
-        .nextSong{
-            float:right;
+
+        .nextSong {
+            float: right;
+        }
+
+        #track_art, #track_art img {
+            height: 300px;
+            width: 300px;
         }
     </style>
 
 </head>
 
 <body>
-
-<div id="box">
-    <div id="media"></div>
-    <br>
-    <a href="#" class="btn btn--m btn--black prevSong">Prev</a>
-    <a href="#" class="btn btn--m btn--black nextSong">Next</a>
-</div>
-
-
+<h1 id="header">FMA-R</h1>
+    <div id="box">
+        <div id="media"><?php require('get_track.php') ?></div>
+        <br>
+        <a href="#" class="btn btn--m btn--black prevSong">Prev</a>
+        <a href="#" class="btn btn--m btn--black nextSong">Next</a>
+    </div>
 
 <script>
-    $(document).ready(function(){
-        $('.nextSong').on('click',function(e){
+    $(document).ready(function () {
+        $('.nextSong').on('click', function (e) {
+
             $.ajax({
-                type:'POST',
-                url:'get_track.php',
-                success: function(media){
-                    $('#media').html(media)
+                type: 'POST',
+                url: 'get_track.php',
+                success: function (media) {
+                    $('#media').html(media);
                 }
             });
+
         })
+
+//        $('html').css({
+//            "background": "url(" + $('#track_art img').attr('src') + ") no-repeat center center fixed",
+//            "-webkit-background-size": "cover",
+//            "-moz-background-size": "cover",
+//            "-o-background-size": "cover",
+//            "background-size": "cover"
+//        })
+    });
+
+    $(document).ajaxStart(function () {
+        $('#track_art img').fadeOut();
+    });
+    //
+    $(document).ajaxStop(function () {
+
+    });
+
+    $(document).ajaxError(function (e, jqxhr, settings, exception) {
+        alert(exception);
     })
+
+
 </script>
 </body>
 </html>
