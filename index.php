d0eef1b1590926999c4138054de6ed6306252b81
chadlol <chadohl@gmail.com>
2015-03-19 01:24:42 -0700

    Random tracks
    Now finds and plays random track upon refresh

--- index.php
+++ index.php
@@ -1,12 +1,52 @@
-<?php
-$url = 'http://www.worldweatheronline.com/feed/weather.ashx?q=schruns,austria&format=json&num_of_days=5&key=8f2d1ea151085304102710%22';
-$content = file_get_contents($url);
-$json = json_decode($content, true);
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>FMA-r</title>
+    <style>
+        #box{
+            width: 300px;
+            height: 50px;
+            margin: auto;
+            position: absolute;
+            top: 0; left: 0; bottom: 0; right: 0;
+        }
+    </style>
+
+</head>
+
+<body>
+
+
+    <?php
+
+    $page = rand(1,4057);
+    $track_id = rand(0, 19);
+
+    $url = "http://freemusicarchive.org/api/get/tracks.json?api_key=7Y2HJLL7FFMODARC&page=$page";
+    $content = file_get_contents($url);
+    $json = json_decode($content, true);
+
+    $track_id = $json['dataset'][$track_id]['track_id'];
+
+    $player = <<<EOD
+    <div id="box">
+    <object width="300" height="50"><param name="movie" value="http://freemusicarchive.org/swf/trackplayer.swf"/>
+        <param name="flashvars" value="track=http://freemusicarchive.org/services/playlists/embed/track/$track_id.xml"/>
+        <param name="allowscriptaccess" value="sameDomain"/>
+        <embed type="application/x-shockwave-flash" src="http://freemusicarchive.org/swf/trackplayer.swf" width="300" height="50" flashvars="track=http://freemusicarchive.org/services/playlists/embed/track/$track_id.xml" allowscriptaccess="sameDomain" />
+    </object>
+EOD;
+
+    echo $player;
+
 ?>
 
 
-<object width="300" height="50"><param name="movie" value="http://freemusicarchive.org/swf/trackplayer.swf"/>
-<param name="flashvars" value="track=http://freemusicarchive.org/services/playlists/embed/track/108267.xml"/>
-<param name="allowscriptaccess" value="sameDomain"/>
-<embed type="application/x-shockwave-flash" src="http://freemusicarchive.org/swf/trackplayer.swf" width="300" height="50" flashvars="track=http://freemusicarchive.org/services/playlists/embed/track/108267.xml" allowscriptaccess="sameDomain" />
-</object>
+
+
+</body>
+</html>
+
